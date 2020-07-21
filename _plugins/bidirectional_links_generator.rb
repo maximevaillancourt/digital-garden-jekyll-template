d8b794908ffb54f2b9be1d2fa96beda573b76380
Maxime Vaillancourt <mv@maximevaillancourt.com>
2020-07-21 17:57:36 -0400

    Identify backlinks after fully completing the Roam/wiki link syntax conversion

--- _plugins/bidirectional_links_generator.rb
+++ _plugins/bidirectional_links_generator.rb
@@ -6,6 +6,8 @@ class BidirectionalLinksGenerator < Jekyll::Generator
 
     all_docs = all_notes + all_pages
 
+    # Convert all Wiki/Roam-style double-bracket link syntax to plain HTML
+    # anchor tag elements (<a>) with "internal-link" CSS class
     all_docs.each do |current_note|
       all_docs.each do |note_potentially_linked_to|
         current_note.content = current_note.content.gsub(
@@ -13,7 +15,10 @@ class BidirectionalLinksGenerator < Jekyll::Generator
           "<a class='internal-link' href='#{note_potentially_linked_to.url}'>#{note_potentially_linked_to.data['title']}</a>"
         )
       end
+    end
 
+    # Identify note backlinks and add them to each note
+    all_notes.each do |current_note|
       notes_linking_to_current_note = all_notes.filter do |e|
         e.content.include?(current_note.url)
       end
