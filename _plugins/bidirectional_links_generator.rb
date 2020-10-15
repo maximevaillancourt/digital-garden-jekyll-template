1a0a706556a6c2565e6406a9521fcb892b1e0806
Maxime Vaillancourt <mv@maximevaillancourt.com>
2020-10-15 07:25:03 -0400

    Style invalid bracket links (grey them out)

--- _plugins/bidirectional_links_generator.rb
+++ _plugins/bidirectional_links_generator.rb
@@ -18,6 +18,19 @@ class BidirectionalLinksGenerator < Jekyll::Generator
           "<a class='internal-link' href='#{note_potentially_linked_to.url}'>#{note_potentially_linked_to.data['title']}</a>"
         )
       end
+
+      # At this point, all remaining double-bracket-wrapped words are
+      # pointing to non-existing pages, so let's turn them into disabled
+      # links by greying them out and changing the cursor
+      current_note.content = current_note.content.gsub(
+        /\[\[?(.*)\]\]/i, # match on the remaining double-bracket links
+        <<~HTML.chomp     # replace with this HTML (\\1 is what was inside the brackets)
+          <span title='There is no note that matches this title.' class='invalid-link'>
+            <span class='invalid-link-brackets'>[[</span>
+            \\1
+            <span class='invalid-link-brackets'>]]</span></span>
+        HTML
+      )
     end
 
     # Identify note backlinks and add them to each note
