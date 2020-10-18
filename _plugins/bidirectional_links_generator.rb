821c020b5af2ab4c3437d4ae2a93aee8690973b9
Josh Rogers <polus@tuta.io>
2020-10-18 17:26:35 +0100

    Initial commit failed on build - adjusted.

--- _plugins/bidirectional_links_generator.rb
+++ _plugins/bidirectional_links_generator.rb
@@ -66,6 +66,7 @@ class BidirectionalLinksGenerator < Jekyll::Generator
   end
 
   def note_id_from_note(note)
-    note.data['title'].gsub!(/\W+/, '').to_i(36).to_s
+    note.data['title'].gsub!(/\W+/, ' ')
+    note.data['title'].delete(' ').to_i(36).to_s
   end
 end
